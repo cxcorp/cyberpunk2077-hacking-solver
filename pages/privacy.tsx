98fcd05c480e89bd7124146f7eca4754198486d7
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2021-01-06 01:25:29 +0200

    Update Privacy Policy about anonymous statistics

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -26,13 +26,42 @@ const PrivacyPage = () => (
                 <a href="https://vercel.com/legal/privacy-policy">
                   Vercel's Privacy Policy
                 </a>{" "}
-                applies. This website does not gather information about its
-                visitors, other than the information gathered by Vercel.
+                applies.
+              </p>
+              <p>
+                This app gathers the following anonymous analytics to determine
+                areas which need improvement:
+                <ul>
+                  <li>
+                    <a href="https://web.dev/vitals/" rel="noopener">
+                      Web Vitals
+                    </a>{" "}
+                    such as Largest Contentful Paint, First Input Delay and
+                    Cumulative Layout Shift
+                  </li>
+                  <li>
+                    Page loads: user agent and page path (to see if there are
+                    browsers that the app needs to optimize for)
+                  </li>
+                </ul>
+              </p>
+              <p>
+                Additionally, anonymous statistics are gathered about the
+                solver's performance, such as:
+                <ul>
+                  <li>Buffer size</li>
+                  <li>Matrix size (n*n)</li>
+                  <li>
+                    Solution success (how many sequences were entered and how
+                    many could be solved for)
+                  </li>
+                </ul>
               </p>
               <p>
                 Any possible changes to the site's privacy policy will be
                 updated on this page.
               </p>
+              <p>Last updated: 2021-01-06</p>
 
               <hr />
               <p>
